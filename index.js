d746a5c10f2013258ecd802d3b41e11995bac268
Christian Wapppler <22932267+Ch3ri0ur@users.noreply.github.com>
2020-06-18 12:18:40 +0200

    sync

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
-const SerialPort = require('serialport')
-const Readline = require('@serialport/parser-readline')
-const server = require("http").createServer();
-const io = require("socket.io")(server);
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const server = require('http').createServer();
+const io = require('socket.io')(server);
 const port = new SerialPort('/dev/ttyACM0', {
-    baudRate: 115200
-})
-const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+    baudRate: 115200,
+});
+const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
 
-
-io.on("connection", (socket) => {
-    console.log("connected to: " + socket.client.id)
+io.on('connection', (socket) => {
+    console.log('connected to: ' + socket.client.id);
     // socket.on("event", (data) => {
     //     console.log(data);
     // });
     parser.on('data', (data) => {
-        if (data.startsWith("I")) {
-            console.log(data)
-            return
+        if (data.startsWith('I')) {
+            console.log(data);
+            return;
         }
-	// console.log(data)
-        socket.emit("measurements", Date.now() + "," + data)
+        // console.log(data)
+        socket.emit('measurements', Date.now() + ',' + data);
     });
-    socket.on("disconnect", () => {
-        console.log("disconnected");
+    socket.on('disconnect', () => {
+        console.log('disconnected');
         socket.removeAllListeners();
         parser.removeAllListeners();
         // socket.disconnect(close = true)
-
     });
-    socket.on("settings", (data) => {
-        port.write(data.join(",") + "T")
-        console.log(data)
-    })
+
+    socket.on('settings', (data) => {
+        port.write(data.join(',') + 'T');
+        console.log(data);
+    });
 });
-server.listen(3000, "0.0.0.0");
+server.listen(3000, '0.0.0.0');
