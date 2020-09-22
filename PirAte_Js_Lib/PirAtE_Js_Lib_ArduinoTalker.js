0331736d362fdb8462c0d764d0ac16c831552d36
Maximilian <maximilian.koch@online.de>
2020-09-22 13:30:11 +0200

    Ardutalker lib for conversion

---
+++ PirAte_Js_Lib/PirAtE_Js_Lib_ArduinoTalker.js
@@ -0,0 +1,18 @@
+//##MSGTypes## ASCII
+//R RequestData
+//M Message
+//I Int
+//U Uint
+//L Long
+//u Ulong
+//F Float
+//D Double
+//C Char
+//S String/Chararray with /0 as end
+//B Byte
+//b bool
+
+//##DELIMITER##
+//0xff P i r A t E \n
+
+function CastArduMsg(ArduMSg)
