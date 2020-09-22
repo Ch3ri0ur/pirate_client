3519e84b2e0fcb3a5202551de7bcf9aa35cd5e10
Maximilian <maximilian.koch@online.de>
2020-09-22 10:39:17 +0200

    Arduino Read  improved

--- PirAtE_Arduino_Lib/PirAtE.h
+++ PirAtE_Arduino_Lib/PirAtE.h
@@ -335,15 +335,25 @@ byte PirAtE_DATA_RECEIVE_BUFFER[PirAtE_Serial_Buffer_Size];
         if(bytes > 0 && PirAtE_DATA_RECEIVE_BUFFER[0] - PirAtE_MSG_DATAID_OFFSET < PirAtE_ReceiveMsg_Amount)\
         {\
           int msgID = PirAtE_DATA_RECEIVE_BUFFER[0] - PirAtE_MSG_DATAID_OFFSET;\
-          if(PirAtE_DATA_RECEIVE_DATATYPE_MASK[msgID] == PirAtE_MSG_DATATYPE_STRING || bytes >= PirAtE_MSG_DATAID_LENGTH+PirAtE_DATA_RECEIVE_DATASIZE[msgID])\
+          if(bytes <= PirAtE_MSG_DATAID_LENGTH+PirAtE_DATA_RECEIVE_DATASIZE[msgID])\
           {\
             receiveCount++;\
-            for (int j = 0; j < PirAtE_DATA_RECEIVE_DATASIZE[msgID];j++)\
+            if(PirAtE_DATA_RECEIVE_DATATYPE_MASK[msgID] == PirAtE_MSG_DATATYPE_STRING)\
             {\
-              PirAtE_DATA_RECEIVE_ADRESSES[msgID][j] = PirAtE_DATA_RECEIVE_DATASIZE[j+1];\
-              if(j == bytes||(PirAtE_DATA_RECEIVE_DATASIZE[j+1]== PirAtE_CHARARRAY_END && PirAtE_DATA_RECEIVE_DATATYPE_MASK[msgID] == PirAtE_MSG_DATATYPE_STRING))\
+              for (int j = 0; j < PirAtE_DATA_RECEIVE_DATASIZE[msgID];j++)\
               {\
-                break;\
+                PirAtE_DATA_RECEIVE_ADRESSES[msgID][j] = PirAtE_DATA_RECEIVE_DATASIZE[j+1];\
+                if(j == bytes||(PirAtE_DATA_RECEIVE_DATASIZE[j+1]== PirAtE_CHARARRAY_END && PirAtE_DATA_RECEIVE_DATATYPE_MASK[msgID] == PirAtE_MSG_DATATYPE_STRING))\
+                {\
+                  break;\
+                }\
+              }\
+            }\
+            else\
+            {\
+              for (int j = 0; j < PirAtE_DATA_RECEIVE_DATASIZE[msgID];j++)\
+              {\
+                PirAtE_DATA_RECEIVE_ADRESSES[msgID][j] = PirAtE_DATA_RECEIVE_DATASIZE[j+1];\
               }\
             }\
           }\
